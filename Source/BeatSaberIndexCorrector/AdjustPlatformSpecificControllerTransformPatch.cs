673070e0213fed68d660231a97d8a25addea3b35
Nicolas Gnyra <nicolas@gnyra.com>
2019-11-25 17:31:32 -0500

    Initial commit

---
+++ Source/BeatSaberIndexCorrector/AdjustPlatformSpecificControllerTransformPatch.cs
@@ -0,0 +1,30 @@
+﻿using Harmony;
+using UnityEngine;
+
+namespace BeatSaberIndexCorrector
+{
+    [HarmonyPatch(typeof(VRPlatformHelper))]
+    [HarmonyPatch("AdjustPlatformSpecificControllerTransform", MethodType.Normal)]
+    public class AdjustPlatformSpecificControllerTransformPatch
+    {
+        public static bool Prefix(Transform transform, VRPlatformHelper __instance)
+        {
+            if (__instance.vrPlatformSDK != VRPlatformHelper.VRPlatformSDK.OpenVR)
+            {
+                return true;
+            }
+
+            OpenVRHelper openVRHelper = Traverse.Create(__instance).Field("_openVRHeper").GetValue<OpenVRHelper>();
+
+            if (openVRHelper.vrControllerManufacturerName != OpenVRHelper.VRControllerManufacturerName.Valve)
+            {
+                return true;
+            }
+
+            transform.Rotate(15f, 0f, 0f);
+            transform.Translate(0f, -0.03f, 0f);
+
+            return false;
+        }
+    }
+}
---
+++ Source/BeatSaberIndexCorrector/Plugin.cs
@@ -0,0 +1,49 @@
+﻿using Harmony;
+using IPA;
+using UnityEngine.SceneManagement;
+using Logger = IPA.Logging.Logger;
+
+namespace BeatSaberIndexCorrector
+{
+    public class Plugin : IBeatSaberPlugin, IDisablablePlugin
+    {
+        public const string kHarmonyId = "com.nicoco007.beatsaberindexcorrector";
+
+        public static Logger logger;
+
+        private HarmonyInstance _harmonyInstance;
+
+        public void Init(Logger logger)
+        {
+            Plugin.logger = logger;
+        }
+
+        public void OnEnable()
+        {
+            _harmonyInstance = HarmonyInstance.Create(kHarmonyId);
+
+            logger.Info("Applying Index controller patch");
+
+            _harmonyInstance.PatchAll();
+        }
+
+        public void OnDisable()
+        {
+            _harmonyInstance.UnpatchAll(kHarmonyId);
+        }
+
+        public void OnApplicationStart() { }
+
+        public void OnActiveSceneChanged(Scene prevScene, Scene nextScene) { }
+
+        public void OnApplicationQuit() { }
+
+        public void OnFixedUpdate() { }
+
+        public void OnSceneLoaded(Scene scene, LoadSceneMode sceneMode) { }
+
+        public void OnSceneUnloaded(Scene scene) { }
+
+        public void OnUpdate() { }
+    }
+}
